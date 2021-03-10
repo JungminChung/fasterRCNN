46c8dd1fdfb3873f66b96cbcbfbdf5b45c3d99d6
jungminchung <mmismin00@gmail.com>
2021-03-10 16:57:31 +0900

    Modify : get num from prev one, set sub res folder

--- utils/folder_utils.py
+++ utils/folder_utils.py
@@ -4,6 +4,11 @@ def check_dir(dir_path):
     if not os.path.isdir(dir_path):
         os.mkdir(dir_path)
 
-def get_working_dir(args, root_dir='runs'):
-    num_folder = len(os.listdir(root_dir))
-    return f'{args.model}_{str(num_folder)}'
+def get_working_dir_name(root_dir, args):
+    folders = os.listdir(root_dir)
+    num = max([int(f.split('_')[1]) for f in folders if f.split('_')[0] == args.model]) + 1 
+    return f'{args.model}_{str(num)}'
+
+def make_results_folders(working_dir):
+    check_dir(os.path.join(working_dir, 'weights'))
+    check_dir(os.path.join(working_dir, 'test_img'))
