439c8cf037ab15db607c1e1a1e7e9bf30fe9d388
jungminchung <mmismin00@gmail.com>
2021-03-10 17:09:49 +0900

    Modify : train process

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import json
 import tqdm 
 import random
 import argparse
+import math
 
 import torch 
 import torchvision
@@ -14,12 +15,14 @@ from torch.utils.tensorboard import SummaryWriter
 
 from models.detection.faster_rcnn import FasterRCNN
 from models.detection.anchor_utils import AnchorGenerator
+from models.detection.faster_rcnn import fasterrcnn_resnet50_fpn
 
-from datasetSIXray import sixrayDataset 
+from datasets.SIXray import sixrayDataset 
+from datasets.coco import cocoDataset
 from utils.evaluate import evaluate, test
 from utils.collators import collate_sixray
 from utils.feature_extract import get_resent_features
-from utils.folder_utils import check_dir, get_working_dir
+from utils.folder_utils import check_dir, get_working_dir_name, make_results_folders
 
 from torch.nn.parallel import DataParallel as DP
 # from torch.nn.parallel import DistributedDataParallel as DDP
@@ -28,23 +31,24 @@ from torch.nn.parallel import DataParallel as DP
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--epochs', type=int, default=140)
-    parser.add_argument('--batch_size', type=int, default=17)
+    parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--momentum', type=float, default=0.9)
     parser.add_argument('--weight_decay', type=float, default=0.005)
     parser.add_argument('--eval_epoch', type=int, default=5)
     parser.add_argument('--save_epoch', type=int, default=5)
     parser.add_argument('--device', type=str)
+    parser.add_argument('--dataset', type=str, default='sixray', help='sixray / coco')
     parser.add_argument('--model', type=str, default='res34')
-    # parser.add_argument('--model', type=str, default='res50')
+    parser.add_argument('--results_root', type=str, default='runs')
+    parser.add_argument('--anchor_init_size', type=int, default='4')
 
     parser.add_argument('--img_min_size', type=int, default=600)
     parser.add_argument('--img_max_size', type=int, default=1000)
 
     parser.add_argument('--coco_target_path', type=str, 
                         default='../SIXray/eval/coco_annotation/coco_SIXray.json')
-    parser.add_argument('--coco_predic_path', type=str, 
-                        default='../SIXray/eval/coco_annotation/coco_predict.json')
+    parser.add_argument('--coco_predict_name', type=str, default='coco_predict.json')
 
     parser.add_argument('--conf_threshold', type=float, default=0.3)
     
@@ -57,24 +61,38 @@ def parse_args():
 def main():
     args = parse_args()
 
-    working_dir = get_working_dir(args)
+    working_dir_name = get_working_dir_name(args.results_root, args)
+    working_dir = os.path.join(args.results_root, working_dir_name)
+    check_dir(working_dir)
+    print(f'Working Dir : {working_dir}')
+    make_results_folders(working_dir) # 'weights' / 'test_img'
+
+    # update args 
+    args.working_dir = working_dir
+    args.weights_dir = os.path.join(args.working_dir, 'weights')
+    args.test_img_dir = os.path.join(args.working_dir, 'test_img')
 
     # init writer for tensorboard 
-    writer_dir = os.path.join('runs', working_dir)
-    check_dir(writer_dir)
-    writer = SummaryWriter(writer_dir)
-    print(f'Tensorboard info will be saved in \'{writer_dir}\'')
+    writer = SummaryWriter(working_dir)
+    print(f'Tensorboard info will be saved in \'{working_dir}\'')
 
     # save args in run folder 
-    with open(os.path.join(writer_dir, 'args.txt'), 'w') as f: 
+    with open(os.path.join(working_dir, 'args.txt'), 'w') as f: 
         json.dump(args.__dict__, f, indent=4)
 
     args.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     print(args)
 
     # Dataset 
-    train_dataset = sixrayDataset('../SIXray', mode='train')
-    eval_dataset = sixrayDataset('../SIXray', mode='eval')
+    if args.dataset == 'sixray':
+        train_dataset = sixrayDataset('../SIXray', mode='train')
+        eval_dataset = sixrayDataset('../SIXray', mode='eval')
+    elif args.dataset ==  'coco' :
+        train_dataset = cocoDataset('../coco', mode='train')
+        eval_dataset = cocoDataset('../coco', mode='eval')
+    else : 
+        raise RuntimeError('Invalide dataset type')
+
 
     # Dataloader 
     train_loader = DataLoader(dataset=train_dataset, batch_size=args.batch_size, 
@@ -95,10 +113,13 @@ def main():
     else : 
         assert()
 
+    # Anchor size : ((size, size*2, size*4, size*8, size*16), )
+    anchor_size = (tuple(int(args.anchor_init_size * math.pow(2, i)) for i in range(5)), )
+
     backbone = get_resent_features(backbone)
     backbone.out_channels = out_ch
 
-    anchor_generator = AnchorGenerator(sizes=((32, 64, 128, 256, 512),),
+    anchor_generator = AnchorGenerator(sizes=anchor_size,
                                         aspect_ratios=((0.5, 1.0, 2.0),))
     
     roi_pooler = torchvision.ops.MultiScaleRoIAlign(featmap_names=['0'], 
@@ -110,6 +131,10 @@ def main():
                     box_roi_pool=roi_pooler,
                     min_size=args.img_min_size, 
                     max_size=args.img_max_size).to(args.device)
+
+    # if args.model == 'res50fpn':
+    #     model = fasterrcnn_resnet50_fpn(pretrained=True).to(args.device)
+    #     model.rpn.anchor_generator.sizes = ((8,), (16,), (32,), (64,), (128,))
 
     # Optimizer 
     optimizer = optim.SGD(model.parameters(), 
@@ -138,21 +163,20 @@ def main():
             loss_obj = loss_dict['loss_objectness'].item()
             loss_rpn_reg = loss_dict['loss_rpn_box_reg'].item()
 
-            progress.set_description(f'Train {epoch} / {args.epochs}, ' +
-                                    f'Loss : (HC){loss_cls:.3f}, (HR){loss_reg:.3f}, ' +
-                                    f'(RO){loss_obj:.3f}, (RR){loss_rpn_reg:.3f} ')
+            progress.set_description(f'Train {epoch} / {args.epochs}, lr : {optimizer.param_groups[0]['lr']}' +
+                                    f'Loss : [TT]{losses:.3f}, [HC]{loss_cls:.3f}, [HR]{loss_reg:.3f}, ' +
+                                    f'[RO]{loss_obj:.3f}, [RR]{loss_rpn_reg:.3f} ')
             
         if epoch % args.save_epoch == 0 : 
-            save_dir = os.path.join(args.model_save_folder, working_dir)
-            check_dir(save_dir)
             torch.save(model.state_dict(), 
-                       os.path.join(save_dir, f'{args.model}_{epoch}.ckpt'))
+                       os.path.join(args.weights_dir, f'{args.model}_{epoch}.ckpt'))
                        
         if epoch % args.eval_epoch == 0 : 
             evaluate(model, eval_loader, args)
             if args.test_img_name == '': 
                 image_path = os.path.join(args.test_img_folder, 
                                           random.sample(os.listdir(args.test_img_folder), 1)[0])
+                args.test_img_name = image_path
             test(model, image_path, args, epoch)
         
         ## Tensor Board 
