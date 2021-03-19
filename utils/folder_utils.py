04ceb00fe1137a54e7f49668106940fd72b4b2c8
jungminchung <mmismin00@gmail.com>
2021-03-19 14:27:14 +0900

    Modify : file name rule

--- utils/folder_utils.py
+++ utils/folder_utils.py
@@ -6,7 +6,11 @@ def check_dir(dir_path):
 
 def get_working_dir_name(root_dir, args):
     folders = os.listdir(root_dir)
-    num = max([int(f.split('_')[1]) for f in folders if f.split('_')[0] == args.model]) + 1 
+    num_list = [int(f.split('_')[1]) for f in folders if f.split('_')[0] == args.model]
+    if len(num_list) > 0 : 
+        num = max(num_list) + 1 
+    else : 
+        num = 0
     return f'{args.model}_{str(num)}'
 
 def make_results_folders(working_dir):
